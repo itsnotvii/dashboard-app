6877f5f99b6889c83b92fdab8d2805c648a9394f
itsnotvii <islandcity57@gmail.com>
2026-05-12 23:54:19 -0700

    initial commit, express server and supabase setup

---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const supabase = require('./db')
+app.use(express.json())
+
+const assignments = [
+    {
+        title: 'Lab 3 - Linked Lists',
+        course: 'CS146 Data Structures',
+        due_at: '2026-05-19T23:59:00',
+        points_possible: 100,
+        score: null
+    },
+    {
+        title: 'Cancer Research Essay',
+        course: 'BIO10 Intro To Biology',
+        due_at: '2026-05-14T23:59:00',
+        points_possible: 250,
+        score: null
+    },
+    {
+        title: 'Final Exam Practice',
+        course: 'ISE130 Engineering Statistics',
+        due_at: '2026-05-20T23:59:00',
+        points_possible: 50,
+        score: null
+    }
+]
+
+app.get('/assignments', (req, res) => {
+    res.json(assignments)
+})
+
+app.post('/shifts', async(req, res) => {
+    const { title, location, shift_start, shift_end, notes} = req.body
+
+    const { data, error } = await supabase
+    .from('shifts')
+    .insert([{ title, location, shift_start, shift_end, notes }])
+    .select()
+
+    if (error) {
+        res.status(500).json({ error: error.message })
+    } else {
+        res.status(201).json(data)
+    }
+})
+
+app.get('/shifts', async (req, res) => {
+    const { data, error } = await supabase
+    .from('shifts')
+    .select('*')
+
+    if (error) {
+        res.status(500).json({ error: error.message })
+    } else {
+        res.json(data)
+    }
+})
+
+app.listen(3001, () => { 
+    console.log('Server is running on port 3001')
+})
